61d611d40fb99d7d3cf62381b794cd42f6cabf76
adityakukade32 <adityakukade39@gmail.com>
2024-06-12 16:38:52 +0530

    added project

---
+++ src/components/accordian/data.js
@@ -0,0 +1,27 @@
+const data = [
+  {
+    id: "1",
+    question: "What are the accordian component?",
+    answer:
+      "Accordian comonents are user interface element used for organization",
+  },
+  {
+    id: "2",
+    question: "What are  they used for?",
+    answer: "They are commanly employed in various contexts,including FAQ",
+  },
+  {
+    id: "3",
+    question: "Accordian as a musical instrument",
+    answer:
+      "yes of course, it is very possible to create an accordian component",
+  },
+  {
+    id: "4",
+    question: "can i create an accordian component with the different from",
+    answer:
+      " The accordian is a musical instrument with the keyboard and below",
+  },
+];
+
+export default data;
